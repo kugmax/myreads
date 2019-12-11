5db91f7d9520f7e8594120c75597a5988d94d82b
Maxim Kugatov <maximkugatov@gmail.com>
2019-12-11 16:45:27 +0200

    chore: added scratch

---
+++ backend/src/model/UserBook.ts
@@ -0,0 +1,12 @@
+export interface UserBook {
+  userId: string
+  createdAt: string,
+  updatedAt: string,
+  status: string,
+  bookId: string,
+  title: string,
+  author: string,
+  description: string
+  isbn: string
+  pages: number
+}
---
+++ backend/src/request/SaveBookRequest.ts
@@ -0,0 +1,7 @@
+export interface SaveBookRequest {
+  title: string
+  author: string
+  description: string
+  isbn: string
+  pages: number
+}
